bb32e8d5dd87ff0bf9b7d8329ffafe9d7b0eee74
Pedro Evangelista <pedroevangelista@outlook.com>
2020-04-14 01:13:34 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeValue = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((a, b) => a + b.value, 0);
+
+    const outcomeValue = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((a, b) => a + b.value, 0);
+
+    const total = incomeValue - outcomeValue;
+
+    const balance: Balance = {
+      income: incomeValue,
+      outcome: outcomeValue,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
